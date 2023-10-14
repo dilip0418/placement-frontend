f1d7dbf517a7e542f91528bbe89c500692323ca8
sandeep <8453368444sandy@gmail.com>
2023-10-15 01:54:50 +0530

    created home,about,contact page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/main/home/home.component';
+import { ContactComponent } from './components/main/contact/contact.component';
+import { AboutComponent } from './components/main/about/about.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, ContactComponent, AboutComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
